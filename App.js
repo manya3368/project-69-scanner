208f5b1aeb6ed2695d90aea6c8dce8d9d4854aac
manya3368 <71918637+manya3368@users.noreply.github.com>
2021-07-01 16:21:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ScanScreen from  "./Screens/ScanScreen"
+
+export default class App extends React.Component {
+  render(){
+  return (
+   <ScanScreen/>
+  );
+}
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
